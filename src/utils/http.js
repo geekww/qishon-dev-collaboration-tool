7669df05a19b0a658232bdf6d4f1bb3ab64d679a
wangwei <jike_wangwei@163.com>
2019-05-09 16:03:47 +0800

    更新http

--- src/utils/http.js
+++ src/utils/http.js
@@ -18,8 +18,54 @@ axios.interceptors.request.use((config) => {
 const http = {
     // get请求方法
     async get(url, params = {}) {
-        // let token = await tokenManager.getCilentToken();
-    }
+        let token = await tokenManager.getCilentToken()
+        let company = Storage.get('COMPANYID')
+        let userInfo = Storage.get('USER_INFO')
+        try {
+            if (company !== null) {
+                params.ownCompanyId = company.company_id
+                params.companyId = company.company_id
+            }
+            if (userInfo !== null) {
+                params.usrId = userInfo.usrId
+            }
+        } catch (e) {}
+        // 全局请求加时间戳
+        // params.timeStamp = Date.parse(new Date());
+        return axios.get(url, {
+            params: params,
+            headers: {
+                Authorization: 'Bearer ' + token,
+                ownCompanyId: params.ownCompanyId,
+                'Cache-Control':'no-cache',
+                'Pragma':'no-cache',
+            }
+        })
+    },
+    // post请求方法
+    post: async function (url, data = {}) {
+        let token = await tokenManager.getCilentToken()
+        let company = Storage.get('COMPANYID')
+        let userInfo = Storage.get('USER_INFO')
+        try {
+            if (company !== null) {
+                data.ownCompanyId = company.company_id
+                data.companyId = company.company_id
+            }
+            if (userInfo !== null) {
+                data.usrId = userInfo.usrId
+            }
+        } catch (e) {}
+        return axios.post(url, data,{
+            headers: {
+                Authorization: 'Bearer ' + token,
+                ownCompanyId: data.ownCompanyId,
+            }
+        })
+            .then((res) =>{
+                return res
+            })
+    },
 }
 
 
